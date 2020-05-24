d792703bcf2ca8b0ca7cda1d8434ccedc3c0432e
FlyWyng <51923865+FlyWyng@users.noreply.github.com>
2020-05-24 11:15:35 +0530

    First commit

---
+++ src/robots.js
@@ -0,0 +1,66 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Vishal',
+		username: 'Vishu',
+		email: 'malikvishal@gmail.com'
+	},
+	{
+		id:2,
+		name: 'Preeti',
+		username: 'Situ',
+		email: 'situ.preeti@gmai.com'
+	},
+	{
+		id: 3,
+		name: 'Anjali',
+		username: 'Angel Priya',
+		email: 'anjalimalik73@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Prateek',
+		username: 'Piggy',
+		email: 'prateekpig@gmail.com',
+	},
+	{
+		id: 5,
+		name: 'Lakshay',
+		username: 'Lucky',
+		email: 'luckyLakshay@gmail.com',
+	},
+	{
+		id: 6,
+		name: 'Riddhi',
+		username: 'Riddho',
+		email: 'riddho@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Bhoomit',
+		username: 'Spiderman',
+		email: 'SpidermanBhoom@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Tarun',
+		username: 'Sprinter',
+		email: 'sprintJaat@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'Abhishek Yadav',
+		username: 'Booty',
+		email: 'bootyMeraNaam@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'Negi',
+		username: 'Jumper',
+		email: 'IamJumper@gmail.com'
+	}
+
+
+]
+
+export default robots;
